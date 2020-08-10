7b81424fcc5a0a466066a8d2d969232ccde8e2de
sauravDutt <sauravd069@gmail.com>
2020-08-10 21:54:39 +0530

    Created Notifications

--- src/comps/signinForm.js
+++ src/comps/signinForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { useState } from 'react';
-import Notification from '/notifications';
+import Notification from './notifications';
 
 
 

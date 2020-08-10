6744d9d6d2ee5f32c9a3aaf0f707902b21acf4bb
sauravDutt <sauravd069@gmail.com>
2020-08-11 00:12:36 +0530

    Created Notifications

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -6,7 +6,7 @@ const Title = () => {
       <header className="header-main">
       <h1>Gallery | SD</h1>
         <a href="https://sauravdutt.tech/"><i class="fas fa-arrow-left" id="headerIcontwo"></i></a>
-        <a href = "../cart/index.html"><i class="fas fa-shopping-cart" id="headerIcon"></i></a>
+        <a href = {'../src/cart/index.html'}><i class="fas fa-shopping-cart" id="headerIcon"></i></a>
       </header>
       <h2>Post A Picture</h2>
       <p>Anonymously Post A Picture By Clicking Add+ Button Bellow.....</p>

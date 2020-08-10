fa3d5805f78cf0590552c00c57b858dd12226971
sauravDutt <sauravd069@gmail.com>
2020-08-10 23:08:25 +0530

    Created Notifications

--- src/comps/signinForm.js
+++ src/comps/signinForm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { useState } from 'react';
-import Notification from './notifications';
 
 
 
@@ -112,10 +111,25 @@ const SigninFormArea = () => {
                     </button>
                 </div>
             </div>
-            <Notification 
-                id="notifications"
-                style={ { display:'none'}}
-            />
+            <motion.div 
+                className="backdrop-notification" 
+                initial = {{opacity: 0}}
+                animate = {{ opacity: 1}}
+            >
+
+            <motion.div 
+                className="notificationDiv" 
+                id='notificationsBtn'
+                initial = {{y:"-100vh"}}
+                animate = {{ y: 0}}
+            >
+                <h2><i 
+                        class="fas fa-bell"
+                        style={{color:'#715086'}}
+                    ></i> Notifications</h2>
+            </motion.div>
+
+        </motion.div>
         </div>
     )
 }

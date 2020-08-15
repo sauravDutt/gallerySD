ab14d9c93c6685efb0a5a4cb4a4ebf456022a240
sauravDutt <sauravd069@gmail.com>
2020-08-15 22:48:01 +0530

    Removed Tital

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 const Title = () => {
   return (
-    <div className="title" id="titleTop">
+    <div className="title">
       <header className="header-main">
       <h1>Gallery | SD</h1>
         <a href="https://sauravdutt.tech/"><i class="fas fa-arrow-left" id="headerIcontwo"></i></a>
         <a href = "https://sauravdutt.tech/cart/index.html"><i class="fas fa-shopping-cart" id="headerIcon"></i></a>
       </header>
-      <h2>Post A Picture</h2>
-      <p>Anonymously Post A Picture By Clicking Add+ Button Bellow.....</p>
     </div>
   )
 }

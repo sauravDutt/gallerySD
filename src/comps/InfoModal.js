bcdb8bfc5acff1041a18f88eb5b21ae9049087fb
sauravDutt <sauravd069@gmail.com>
2020-08-15 22:43:50 +0530

    Updated Info

--- src/comps/InfoModal.js
+++ src/comps/InfoModal.js
@@ -19,7 +19,9 @@ const Info = ({setShowInfo}) =>{
                 animate = {{ y: 0}}
             >
                 <h2><i class="fas fa-info-circle" style={{color:'#5c0b92'}}></i> Info</h2>
-                <p></p>
+                <p>
+                    The Gallery is a practice for Reactjs and Google Firebase. For uploading a picture to the gallery just click the <b>Add+</b> button on the Right. You can anonymously post any PNG or JPEG image.
+                </p>
             </motion.div>
         </motion.div>
     )
